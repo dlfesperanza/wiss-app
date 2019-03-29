9cd7f58e6345607d55439cf77804debc2d39c134
Dannah Louise Esperanza <dfesperanza@up.edu.ph>
2019-03-29 22:25:02 +0800

    added bottom-navigation with fragments

---
+++ app/src/main/java/com/example/wiss/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.wiss;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    toolbar.setTitle("News");
+                    fragment = new NewsFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    toolbar.setTitle("Weather");
+                    fragment = new WeatherFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    toolbar.setTitle("Emergency");
+                    fragment = new EmergencyFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new NewsFragment());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar = getSupportActionBar();
+        toolbar.setTitle("News");
+
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+}
